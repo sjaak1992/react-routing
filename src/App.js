ef11b8ec79c3825571fbefda727db55bb1cc1da3
sjaak1992 <syakirahalgadri@gmail.com>
2021-03-24 15:30:32 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ import AppointmentsPage from './pages/Appointments';
 import WhiteningPage from './pages/Whitening';
 import './App.css';
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
 function App() {
-  return (
-    <>
-      <TopMenu />
-      <HomePage />
-      <CavitiesPage />
-      <AppointmentsPage />
-      <WhiteningPage />
-    </>
-  );
+    return (
+        <Router>
+            <TopMenu/>
+            <Switch>
+                <Route exact path="/"><HomePage/></Route>
+                <Route path="/gaatjes"><CavitiesPage/></Route>
+                <Route path="/afspraak-maken"><AppointmentsPage/></Route>
+                <Route path="/tanden-bleken"> <WhiteningPage/></Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
